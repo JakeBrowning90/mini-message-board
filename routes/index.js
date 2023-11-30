25033d52853d998908bbd93398c77446b564ed80
JakeBrowning90 <jacobbrowning90@gmail.com>
2023-11-30 11:26:12 -0500

    Add POST function to form

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 const messages = [
   {
     text: "Hi there!",
-    user: "Amando",
+    user: "JakeBot",
     added: new Date()
   },
   {
     text: "Hello World!",
-    user: "Charles",
+    user: "SarahBot",
     added: new Date()
   }
 ];
@@ -18,5 +18,21 @@ const messages = [
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Message Board', chatLog: messages});
 });
+
+/* GET form page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message Form' });
+});
+
+/* POST form page. */
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageAuthor,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
+
 
 module.exports = router;
